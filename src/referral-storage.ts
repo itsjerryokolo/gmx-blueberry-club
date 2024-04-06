3dbdf094da12d164747513437e0a6fc7f8ee6692
Jerry Okolo <jerrycreative@live.co.uk>
2024-04-06 17:51:52 +0100

    Update const.ts and referral-storage.ts files

--- src/referral-storage.ts
+++ src/referral-storage.ts
@@ -1,17 +1,22 @@
+import { log } from "@graphprotocol/graph-ts";
 import { SetTraderReferralCode as SetTraderReferralCodeEvent } from "../generated/ReferralStorage/ReferralStorage";
 import { ReferralAccount } from "../generated/schema";
 import { BLUEBERRY_REFERRAL_CODE } from "./common/const";
 
 export function handleSetTraderReferralCode(
-  event: SetTraderReferralCodeEvent,
+	event: SetTraderReferralCodeEvent
 ): void {
-  if (event.params.code.toHexString() == BLUEBERRY_REFERRAL_CODE) {
-    let referralAccount = new ReferralAccount(event.params.account);
-    referralAccount.code = event.params.code;
-    referralAccount.blockNumber = event.block.number;
-    referralAccount.blockTimestamp = event.block.timestamp;
-    referralAccount.transactionHash = event.transaction.hash;
+	log.warning("Refferal Code Found---> Account: {}, Code: {}", [
+		event.params.account.toHexString(),
+		event.params.code.toHexString(),
+	]);
+	if (event.params.code.toHexString() == BLUEBERRY_REFERRAL_CODE) {
+		let referralAccount = new ReferralAccount(event.params.account);
+		referralAccount.code = event.params.code;
+		referralAccount.blockNumber = event.block.number;
+		referralAccount.blockTimestamp = event.block.timestamp;
+		referralAccount.transactionHash = event.transaction.hash;
 
-    referralAccount.save();
-  }
+		referralAccount.save();
+	}
 }
