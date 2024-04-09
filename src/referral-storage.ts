7fc79e8e07aa5f8e2987890c5268be41b872c281
Jerry Okolo <jerrycreative@live.co.uk>
2024-04-09 09:35:54 +0100

    add logs

--- src/referral-storage.ts
+++ src/referral-storage.ts
@@ -6,11 +6,12 @@ import { BLUEBERRY_REFERRAL_CODE } from "./common/const";
 export function handleSetTraderReferralCode(
 	event: SetTraderReferralCodeEvent
 ): void {
-	log.warning("Refferal Code Found---> Account: {}, Code: {}", [
-		event.params.account.toHexString(),
-		event.params.code.toHexString(),
-	]);
 	if (event.params.code.toHexString() == BLUEBERRY_REFERRAL_CODE) {
+		log.warning("Refferal Code Found---> Account: {}, Code: {}", [
+			event.params.account.toHexString(),
+			BLUEBERRY_REFERRAL_CODE,
+		]);
+
 		let referralAccount = new ReferralAccount(event.params.account);
 		referralAccount.code = event.params.code;
 		referralAccount.blockNumber = event.block.number;
